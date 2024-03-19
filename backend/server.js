b877999eb5264b7cecda10ec876ecdab51862f7d
vy211 <vipinkumar.y@simplify3x.com>
2024-03-19 16:08:31 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const { errorHandler } = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
-
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser);  
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
@@ -19,5 +26,5 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 page not found');
     }
 })
-
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`server is listning on port ${PORT}`));
